a06d2501853ea2f743f3d22ce39a0e1baa9a3726
Christian Jones <c529j466@yahoo.com>
2022-11-17 00:45:22 -0600

    updated savebooks

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -5,8 +5,18 @@ import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
+import { useQuery } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+
+import { useMutation } from '@apollo/client';
+import { REMOVE_BOOK } from '../utils/mutations';
+
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const { loading, data } = useQuery(GET_ME);
+
+  const [deleteBook, { error }] = useMutation(REMOVE_BOOK);
+
+  const userData = data?.me || {};
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
